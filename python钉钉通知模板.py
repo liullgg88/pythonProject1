acc0b30c71c44fd4a0839bb492204afc104d22ee
lll <546651847@qq.com>
2024-08-21 15:57:02 +0800

    test

--- python钉钉通知模板.py
+++ python钉钉通知模板.py
@@ -94,4 +94,3 @@ if __name__ == '__main__':
     server_name_string="xxxxxx"
     url="https://oapi.dingtalk.com"
     create_dingtalk_message(title, pipeline_name, start_time, end_time, pipeline_run_status, ruleContents, server_name_string, url)
-
