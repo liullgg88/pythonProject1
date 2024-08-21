000f55585fcbee9e3bbe8c9bfc28094e6d18f297
lll <546651847@qq.com>
2024-08-21 15:49:33 +0800

    test

--- zadig调用华为云流水线脚本.py
+++ zadig调用华为云流水线脚本.py
@@ -9,7 +9,12 @@ import time
 import os
 import argparse
 import configparser
+import re
+from dingtalkchatbot.chatbot import DingtalkChatbot
 
+
+webhook = 'https://oapi.dingtalk.com/robot/send?access_token=425ff9a2f17b8143c46a951ad3eba20ff69e9b615fcb7d4b8b5bdf383dd56568'
+ding_talk = DingtalkChatbot(webhook)
 
 def sign_request(http_method, url, headers=None, body=None): # 获取登录认证密钥
     sig = signer.Signer()
@@ -177,17 +182,65 @@ def  get_pipeline_id(project_id, dict_key_string):
     except requests.RequestException as e:
         print(f"Error fetching pipeline ID: {e}")
         return None
-    # try:
-    #     resp = requests.post(req.url, headers=req.headers, data=req.body)
-    #     resp.raise_for_status()  # 如果响应状态码不是 200，则抛出 HTTPError
-    #     data = resp.json()
-    #     for pipeline in data.get('pipelines', []):
-    #         if pipeline['name'].split("-")[-1] == dict_key_string:
-    #             return pipeline['pipeline_id']
-    # except requests.RequestException as e:
-    #     print(f"Error fetching pipeline ID: {e}")
-    #     return None
-def create_dingtalk_message(title, pipeline_name, start_time, end_time, pipeline_run_status, server_name_string, url): # 钉钉消息模板定义
+
+def get_pipeline_step_run_ids(project_id, pipeline_id): #获取流水线详细信息
+    url = f"https://cloudpipeline-ext.cn-east-3.myhuaweicloud.com/v5/{project_id}/api/pipelines/{pipeline_id}/pipeline-runs/detail"
+
+    payload = ""
+    headers = {
+        'Content-Type': 'application/json;charset=UTF-8'
+    }
+
+    # response = requests.request("GET", url, headers=headers, data=payload)
+    req = sign_request("GET", url, headers=headers, body=payload)
+    resp = requests.get(req.url, headers=req.headers)
+    resp.raise_for_status()  # 如果响应状态码不是 200，则抛出 HTTPError
+    data = resp.json()
+    # 假设data中有一个名为'stages'的列表，每个阶段有一个'status'和可能的'post'列表
+    for stage in data.get("stages", []):
+        if stage.get("status") == "FAILED" and stage.get("post"):
+            # 返回第一个失败阶段的第一个后处理的ID
+            return stage["post"][0].get("id")
+
+    # 如果没有找到失败的阶段，则返回None
+    return None
+
+def Get_Step_Out_puts(project_id, pipeline_id, pipeline_run_id, step_run_ids): # 获取流水线步骤执行输出
+    url = f"https://cloudpipeline-ext.cn-east-3.myhuaweicloud.com/v5/{project_id}/api/pipelines/{pipeline_id}/pipeline-runs/{pipeline_run_id}/steps/outputs?step_run_ids={step_run_ids}"
+    payload = ""
+    headers = {
+        'Content-Type': 'application/json;charset=UTF-8'
+    }
+    req = sign_request("GET", url, headers=headers, body=payload)
+    try:
+        resp = requests.get(req.url, headers=req.headers)
+        resp.raise_for_status()  # 抛出异常如果HTTP请求返回了错误状态码
+        data = resp.json()
+        # 检查data中是否包含'step_outputs'键
+        if 'step_outputs' in data:
+            for step_output in data['step_outputs']:
+                if 'output_result' in step_output and step_output['output_result']:
+                    for result in step_output['output_result'][-1]['value']:
+                        if 'ruleResults' in result:
+                            for rule in result['ruleResults']:
+                                if rule['status'] == "FAILED":
+                                    rule_name = rule['ruleName'].split("/")[0]
+                                    step_run_id = rule["stepRunId"]
+
+                                    # 假设fact是metricExecutionResults列表中最后一个元素的fact
+                                    if 'ruleContents' in rule and rule['ruleContents']:
+                                        metric_results = rule['ruleContents'][0].get('metricExecutionResults', [])
+                                        if metric_results:
+                                            fact = metric_results[-1].get('fact', '未知')
+                                            # print(f"失败步骤名称：{rule_name}")
+                                            # print(f"stepRunId: {step_run_id}")
+                                            # print(f"API测试通过率：{fact}")
+                                            return "%s/%s" % (rule_name,fact)
+    except requests.RequestException as e:
+        print(f"请求错误：{e}")
+    except Exception as e:
+        print(f"处理错误：{e}")
+def create_dingtalk_message(title, pipeline_name, start_time, end_time, pipeline_run_status, ruleContents, server_name_string, url): # 钉钉消息模板定义
     status_color = {
         "COMPLETED": "#32CD32",
         "FAILED": "#FF0000",
@@ -195,15 +248,59 @@ def create_dingtalk_message(title, pipeline_name, start_time, end_time, pipeline
         "RUNNING": "#32CD32"
     }
     color = status_color.get(pipeline_run_status, "#FFA500")
+    module_dict = {
+        "ShenYu网关检查": ["15206115285"],
+        "健康检查-fund-server": ["15206115285"],
+        "健康检查-Id-server": ["15206115285"],
+        "健康检查-Base-server": ["15206115285"],
+        "健康检查-amazon-wms": ["15206115285"],
+        "海外-shenyu网关健康检查": ["15206115285"],
+        "wecom": ["15728046338"],
+        "oms": ["15728046338"],
+        "anth-server": ["18515343059"],
+        "sms-center": ["18515343059"],
+        "delivery-server": ["18515343059"],
+        "独享-amazonretail": ["15728046338"],
+        "独享-pos": ["15728046338"],
+        "独享-水星b端": ["15728046338"],
+        "独享-水星小程序": ["15728046338"],
+        "独享argus": ["15728046338"],
+        "独享-amazonCenter-库存": ["18368404677"],
+        "独享-amazonWhs": ["18368404677"],
+        "独享-amazonCenter-采购": ["18368404677"],
+        "独享-amazonCenter-配送": ["18368404677"],
+        "user-center": ["18515343059"],
+        "amazon-report": ["18515343059"],
+        "minipay": ["18515343059"],
+        "allpay-web": ["18515343059"],
+        "finance-server": ["18515343059"],
+        "enivoice": ["18515343059"],
+        "wms-标品": ["18368404677"],
+        "wms-非标": ["18368404677"],
+        "fund-server": ["18368404677"],
+    }
+    module_name = ruleContents.split("/")[0]
+    fate = ruleContents.split("/")[-1]
     if title=="结束":
-        text = f"### CodeArts Pipeline执行{title}\n\n" \
-               f"> **流水线名称:** {pipeline_name}\n\n" \
-               f"> **开始时间:** {start_time}\n\n" \
-               f"> **结束时间:** {end_time}\n\n" \
-               f"> **运行状态:** <font color={color}>{pipeline_run_status}</font>\n\n" \
-               f"> **链接:** [**查看详情**]({url})\n\n" \
-               f"> **此流水线由{server_name_string}等触发**\n\n" \
-               f"> **自动化测试** "
+        if pipeline_run_status=="FAILED":
+            text = f"### CodeArts Pipeline执行{title}\n\n" \
+                   f"> **流水线名称:** {pipeline_name}\n\n" \
+                   f"> **开始时间:** {start_time}\n\n" \
+                   f"> **结束时间:** {end_time}\n\n" \
+                   f"> **运行状态:** <font color={color}>{pipeline_run_status}</font>\n\n" \
+                   f"> **失败原因:** <font color={color}>{module_name}套件失败，API测试通过率{fate}</font>\n\n" \
+                   f"> **链接:** [**查看详情**]({url})\n\n" \
+                   f"> **此流水线由{server_name_string}等触发**\n\n" \
+                   f"> **自动化测试**  \n\n"
+        else:
+            text = f"### CodeArts Pipeline执行{title}\n\n" \
+                   f"> **流水线名称:** {pipeline_name}\n\n" \
+                   f"> **开始时间:** {start_time}\n\n" \
+                   f"> **结束时间:** {end_time}\n\n" \
+                   f"> **运行状态:** <font color={color}>{pipeline_run_status}</font>\n\n" \
+                   f"> **链接:** [**查看详情**]({url})\n\n" \
+                   f"> **此流水线由{server_name_string}等触发**\n\n" \
+                   f"> **自动化测试**  \n\n"
     else:
         text = f"### CodeArts Pipeline执行{title}\n\n" \
                f"> **工作流名称:** {pipeline_name}\n\n" \
@@ -211,25 +308,24 @@ def create_dingtalk_message(title, pipeline_name, start_time, end_time, pipeline
                f"> **运行状态:** <font color={color}>{pipeline_run_status}</font>\n\n" \
                f"> **工作流链接:** [**查看详情**]({url})\n\n" \
                f"> **此流水线由{server_name_string}等触发**\n\n" \
-               f"> **自动化测试** "
-    return {
-        "msgtype": "markdown",
-        "markdown": {
-            "title": "Spark Monitor",
-            "text": text
-        }
-    }
+               f"> **自动化测试** \n\n"
+    notification_phone_list = ["17612843195"]
+    result = ding_talk.send_markdown(title, text, at_mobiles=notification_phone_list, is_at_all=False)
+    if result['errmsg'] == 'ok':
+        print('通知成功')
+    else:
+        print(f'通知失败: {result["errmsg"]}')
 
 
-def send_dingtalk_message(webhook_url, message):  # 发送钉钉消息
-    headers = {'Content-Type': 'application/json'}
-    try:
-        response = requests.post(webhook_url, data=json.dumps(message), headers=headers)
-        response.raise_for_status()
-        return response.text
-    except requests.exceptions.RequestException as e:
-        print("Error sending DingTalk message:", e)
-        sys.exit(1)
+# def send_dingtalk_message(webhook_url, message):  # 发送钉钉消息
+#     headers = {'Content-Type': 'application/json'}
+#     try:
+#         response = requests.post(webhook_url, data=json.dumps(message), headers=headers)
+#         response.raise_for_status()
+#         return response.text
+#     except requests.exceptions.RequestException as e:
+#         print("Error sending DingTalk message:", e)
+#         sys.exit(1)
 
 def check_process_exists(pid):  # 检查之前是否有本脚本运行的进程，如果有则等待
     """Check if a process with the given PID exists."""
@@ -267,7 +363,6 @@ if __name__ == '__main__':
     # webhook_url = 'https://oapi.dingtalk.com/robot/send?access_token=d1b408bc9c147c819da6915baac84ac677c2f3cfbb8d2a8f7530b84e4e4974a8'
     webhook_url = 'https://oapi.dingtalk.com/robot/send?access_token=425ff9a2f17b8143c46a951ad3eba20ff69e9b615fcb7d4b8b5bdf383dd56568'
     parser = argparse.ArgumentParser(description='zadig调用codearts流水线信息的脚本')
-
     # 添加参数
     parser.add_argument('--u', '--u', type=str, required=True, help='商户名称')
     parser.add_argument('--p', '--p', type=int, required=True, help='前面运行的本脚本PID进程')
@@ -307,14 +402,13 @@ if __name__ == '__main__':
         pipeline_name = "正式环境-更新"
         pipeline_status = "没有对应的标签,请给流水线添加对应的标签"
         end_time = ""
-        message = create_dingtalk_message(title,
+        create_dingtalk_message(title,
                                           pipeline_name,
                                           start_time,
                                           end_time,
                                           pipeline_status,
                                           service_name_string,
                                           task_url)
-        response_text = send_dingtalk_message(webhook_url, message)
         sys.exit(1)
     get_pipeline_running_lists(project_id)
     pipeline_run_id = run_pipeline(pipeline_id, project_id, choose_jobs, choose_stages)
@@ -323,25 +417,39 @@ if __name__ == '__main__':
         workflow_name="正式环境-更新"
         pipeline_status="RUNNING"
         end_time=""
-        message = create_dingtalk_message(title,
-                                          workflow_name,
-                                          start_time,
-                                          end_time,
-                                          pipeline_status,
-                                          service_name_string,
-                                          task_url)
-        response_text = send_dingtalk_message(webhook_url, message)
+        ruleContents = ""
+        create_dingtalk_message(title,
+                              workflow_name,
+                              start_time,
+                              end_time,
+                              pipeline_status,
+                              ruleContents,
+                              service_name_string,
+                              task_url)
         pipeline_status = get_pipeline_status(project_id, pipeline_id, pipeline_run_id)
         if pipeline_status:
-            title="结束"
-            message = create_dingtalk_message(title,
-                                              pipeline_status['pipeline_name'],
-                                              pipeline_status['start_time'],
-                                              pipeline_status['end_time'],
-                                              pipeline_status['pipeline_run_status'],
-                                              service_name_string,
-                                              pipeline_url=f"https://devcloud.cn-east-3.huaweicloud.com/cicd/project/{project_id}/pipeline/detail/{pipeline_id}/{pipeline_run_id}?v=1")
-            response_text = send_dingtalk_message(webhook_url, message)
+            title = "结束"
+            if pipeline_status['pipeline_run_status']=="FAILED":
+                step_run_ids = get_pipeline_step_run_ids(project_id, pipeline_id)
+                ruleContents=Get_Step_Out_puts(project_id, pipeline_id, pipeline_run_id, step_run_ids)
+                create_dingtalk_message(title,
+                                      pipeline_status['pipeline_name'],
+                                      pipeline_status['start_time'],
+                                      pipeline_status['end_time'],
+                                      pipeline_status['pipeline_run_status'],
+                                      ruleContents,
+                                      service_name_string,
+                                      url=f"https://devcloud.cn-east-3.huaweicloud.com/cicd/project/{project_id}/pipeline/detail/{pipeline_id}/{pipeline_run_id}?v=1")
+            else:
+                ruleContents = ""
+                create_dingtalk_message(title,
+                                          pipeline_status['pipeline_name'],
+                                          pipeline_status['start_time'],
+                                          pipeline_status['end_time'],
+                                          pipeline_status['pipeline_run_status'],
+                                          ruleContents,
+                                          service_name_string,
+                                          url=f"https://devcloud.cn-east-3.huaweicloud.com/cicd/project/{project_id}/pipeline/detail/{pipeline_id}/{pipeline_run_id}?v=1")
             if pipeline_status['pipeline_run_status'] != "COMPLETED":
                 print("Pipeline run did not complete successfully. Exiting.")
                 sys.exit(1)
